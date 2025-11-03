77d52551695ce3d37a75a523f19f273d6271d961
aeon <jerry73204@gmail.com>
2025-11-04 04:04:16 +0800

    Use BEST_EFFORT output

--- zed_components/src/zed_camera/src/zed_camera_component_main.cpp
+++ zed_components/src/zed_camera/src/zed_camera_component_main.cpp
@@ -66,7 +66,7 @@ namespace stereolabs
 ZedCamera::ZedCamera(const rclcpp::NodeOptions & options)
 : Node("zed_node", options),
   mThreadStop(false),
-  mQos(QOS_QUEUE_SIZE),
+  mQos(rclcpp::QoS(QOS_QUEUE_SIZE).best_effort()),
   mAiInstanceID(0),
   mDiagUpdater(this),
   mImuTfFreqTimer(get_clock()),
